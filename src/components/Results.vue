8db630eca0a860cd47e390815cba4829475b0bf7
SaneStreet <MichaelHansenIT@live.dk>
2021-07-01 13:21:41 +0200

    Initial commit

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+    <p>Reaction time : {{ score }} ms.</p>
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "Like a freakin' ninja my dude"
+        } else if (this.score < 400) {
+            this.rank = "Rapid flexes bro"
+        } else {
+            this.rank = ".. bruh, u a snail"
+        }
+    }
+}
+</script>
+
+<style>
+.rank {
+    font-size: 1.4em;
+    color: orangered;
+    font-weight: bold;
+}
+</style>
